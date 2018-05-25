f78fa406d355871229f543181b59dba564c2c5f8
Cecilia <ceciliapigeolet@gmail.com>
2018-05-25 11:26:32 +0200

    first commit

---
+++ src/main/java/com/cess/LastSpring/LastSpring/LastSpringApplication.java
@@ -0,0 +1,12 @@
+package com.cess.LastSpring.LastSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cess/LastSpring/LastSpring/LastSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cess.LastSpring.LastSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LastSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
